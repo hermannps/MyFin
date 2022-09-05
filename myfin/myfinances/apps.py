2471095f4db397cf3d998092a998a58c96325f0a
hermannps <hermannps@gmail.com>
2022-09-05 12:06:41 -0300

    git config --global user.email "hermannps@gmail.com"

---
+++ myfin/myfinances/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyfinancesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myfinances'
---
+++ myfin/myfinances/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+class item_orcamento(models.Model):
+    valor = models.DecimalField(max_lengmax_digits=10, decimal_places=2)
+    valor_previsto = models.BooleanField(default=True)
+    mes = models.IntegerChoices()
+    categoria_escolha = [
+        ('Receita', (
+                ('Renda de Investimentos', 'Renda de Investimentos'),
+                ('Salario de Hermann', 'Salario de Hermann'),
+                ('Refeicao de Hermann', 'Refeicao de Hermann'),
+                ('Salario de Carla', 'Salario de Carla'),
+                ('Refeicao de Carla', 'Refeicao de Carla'),
+                ('Processo do Alphaville', 'Processo do Alphaville'),
+            )
+        ),
+        ('Despesa', (
+                ('Financiamento Bancario','Financiamento Bancario'),
+                ('Plano de Previdencia','Plano de Previdencia'),
+                ('Empregada','Empregada'),
+                ('NuBank Hermann','NuBank Hermann'),
+                ('Nubank Carla','Nubank Carl'),
+                ('Escola','Escola'),
+                ('Seguro','Seguro'),
+                ('Internet','Internet'),
+                ('Energia Celpe','Energia Celpe'),
+                ('Energia Cosern','Energia Cosern'),
+                ('INSS','INSS'),
+                ('Celular Hermann','Celular Hermann'),
+                ('Celular Carla','Celular Carla'),
+                ('Condominio Maria Borba','Condominio Maria Borba'),
+                ('Condominio Alphaville','Condominio Alphaville'),
+                ('IPTU+BOMB','IPTU+BOMB'),
+                ('IPVA+MULTA','IPVA+MULTA'),
+                ('Alimentacao','Alimentacao'),
+                ('Agua/Esgoto Pipa','Agua/Esgoto Pipa'),
+                ('Advogado/Outros','Advogado/Outros'),
+                ('Transf. Conta Invest','Transf. Conta Invest'),
+            )
+        ),
+    ]
+    categoria  = models.CharField(
+        max_length=50,
+        choices=categoria_escolha,
+    )
