f005c566fa6f17310593cd84f175eb3d54ae942e
hermannps <hermannps@gmail.com>
2022-09-05 18:36:14 -0300

    .

--- myfin/myfinances/views.py
+++ myfin/myfinances/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import item_orcamento
+from .models import item_orcamento,Todo
 
 # Create your views here.
 def index(request):
+    todos = Todo.objects.all()
     return render(request,"base.url", {"itens_orcamento": item_orcamento})
